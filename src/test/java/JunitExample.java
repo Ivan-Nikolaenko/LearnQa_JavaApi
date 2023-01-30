a0f3493e28c9005fac472b92cefe0c8322d73dc2
Ivan-Nikolaenko <inikolaenko@leantech.ai>
2023-01-30 18:43:50 +0600

    Добавил примеры

--- src/test/java/JunitExample.java
+++ src/test/java/JunitExample.java
@@ -7,19 +7,19 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class JunitExample {
     @Test
-    public void testRestAssuredOk(){
+    public void testPassed200() {
         Response response = RestAssured
                 .get("https://playground.learnqa.ru/api/map")
                 .andReturn();
-//        assertTrue(response.statusCode()==201,"Unexpected status code");
+//        assertTrue(response.statusCode()==200,"Unexpected status code");
         assertEquals(200,response.statusCode(),"Unexpected status code");
     }
     @Test
-    public void testRestAssuredFail(){
+    public void testFailed404() {
         Response response = RestAssured
                 .get("https://playground.learnqa.ru/api/map2")
                 .andReturn();
-//        assertTrue(response.statusCode()==201,"Unexpected status code");
-        assertEquals(200,response.statusCode(),"Unexpected status code");
+//        assertTrue(response.statusCode()==200,"Unexpected status code");
+        assertEquals(404,response.statusCode(),"Unexpected status code");
     }
 }
