c4971f345a7f48ab3885e7067fd6aeb37c41861e
Ivan-Nikolaenko <inikolaenko@leantech.ai>
2023-02-03 17:13:49 +0600

    ++

--- src/test/java/tests/UserAuthExampleBeforeEach.java
+++ src/test/java/tests/UserAuthExampleBeforeEach.java
@@ -1,7 +1,10 @@
+package tests;
+
 import io.restassured.RestAssured;
 import io.restassured.http.Headers;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
+import lib.BaseTestCase;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -11,14 +14,13 @@ import java.util.Map;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class UserAuthExampleBeforeEach {
+public class UserAuthExampleBeforeEach extends BaseTestCase {
     String cookie;
     String header;
     int userIdOnAuth;
-    @BeforeEach
 
-    @Test
-    public void testAuthUser() {
+    @BeforeEach
+    public void loginUser() {
         Map<String, String> authData = new HashMap<>();
         authData.put("email", "vinkotov@example.com");
         authData.put("password", "1234");
@@ -28,27 +30,23 @@ public class UserAuthExampleBeforeEach {
                 .body(authData)
                 .post("https://playground.learnqa.ru/api/user/login")
                 .andReturn();
+        this.cookie = responseGetAuth.getCookie("auth_sid");
+        this.header = responseGetAuth.getHeader("x-csrf-token");
+        this.userIdOnAuth = responseGetAuth.jsonPath().getInt("user_id");
+    }
 
-        Map<String, String> cookies = responseGetAuth.getCookies();
-        Headers headers = responseGetAuth.getHeaders();
-        int userIdOnAuth = responseGetAuth.jsonPath().getInt("user_id");
+    @Test
+    public void testAuthUser() {
 
-        assertEquals(200, responseGetAuth.statusCode(), "Unexpected status code");
-        assertTrue(cookies.containsKey("auth_sid"),"Response doesn't have 'auth_sid' cookie");
-        assertTrue(headers.hasHeaderWithName("x-csrf-token"),"Response doesn't have 'x-" +
-                "crf-token' header");
-        assertTrue(responseGetAuth.jsonPath().getInt("user_id")>0,"User" +
-                "id shiuld be greater than 0");
-
-        JsonPath responseChackAuth  = RestAssured
+        JsonPath responseChackAuth = RestAssured
                 .given()
-                .header("x-csrf-token",responseGetAuth.getHeader("x-csrf-token"))
-                .cookie("auth_sid",responseGetAuth.getCookie("auth_sid"))
+                .header("x-csrf-token", this.header)
+                .cookie("auth_sid", this.cookie)
                 .get("https://playground.learnqa.ru/api/user/auth")
                 .jsonPath();
 
         int userIdOnCheck = responseChackAuth.getInt("user_id");
-        assertTrue(userIdOnCheck>0,"Unexpecred user id "+userIdOnCheck);
+        assertTrue(userIdOnCheck > 0, "Unexpecred user id " + userIdOnCheck);
 
         assertEquals(
                 userIdOnAuth,
